240b95c9f5671e5120de61d3daa2252bf3af723b
nurulamin092 <mnurulamin092@gmail.com>
2026-01-04 20:31:34 +0600

    create comment router

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { toNodeHandler } from "better-auth/node";
 import { postRouter } from "./modules/post/post.router";
 import { auth } from "./lib/auth";
 import cors from "cors";
+import { commentRouter } from "./modules/comment/comment.router";
 const app: Application = express();
 
 app.use(
@@ -14,5 +15,6 @@ app.use(
 app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use(express.json());
 app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
 
 export default app;
